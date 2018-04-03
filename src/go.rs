00ac03b3addb1f3046aae5067d5908e037e435d5
John-John Tedro <udoprog@tedro.se>
2018-04-03 18:48:16 +0200

    go: support maps, arrays, and interfaces as first-class types

--- src/go.rs
+++ src/go.rs
@@ -1,42 +1,72 @@
 //! Specialization for Go code generation.
 
-use {Custom, Formatter, Quoted, Tokens};
-use std::borrow::Cow;
 use std::collections::BTreeSet;
 use std::fmt::{self, Write};
+use {Cons, Custom, Formatter, Quoted, Tokens};
 
 const SEP: &str = ".";
+
+/// Name of an imported type.
+#[derive(Debug, Clone)]
+pub struct Name<'el> {
+    /// Module of the imported name.
+    module: Option<Cons<'el>>,
+    /// Name imported.
+    name: Cons<'el>,
+}
 
 /// Go token specialization.
 #[derive(Debug, Clone)]
 pub enum Go<'el> {
-    /// An imported name.
-    Imported {
-        /// Module of the imported name.
-        module: Cow<'el, str>,
-        /// Name imported.
-        name: Cow<'el, str>,
+    /// A regular type.
+    Type {
+        /// The name being referenced.
+        name: Name<'el>,
     },
-    /// An local name (same package)
-    Local {
-        /// Name.
-        name: Cow<'el, str>,
+    /// A map, map[<key>]<value>.
+    Map {
+        /// Key of the map.
+        key: Box<Go<'el>>,
+        /// Value of the map.
+        value: Box<Go<'el>>,
     },
+    /// An array, []<inner>.
+    Array {
+        /// Inner value of the array.
+        inner: Box<Go<'el>>,
+    },
+    /// An interface type, interface{}.
+    Interface,
 }
 
 impl<'el> Go<'el> {
-    fn imports<'a>(tokens: &'a Tokens<'a, Self>) -> Option<Tokens<'a, Self>> {
+    fn type_imports<'a, 'b: 'a>(go: &'b Go<'b>, modules: &'a mut BTreeSet<&'b str>) {
         use self::Go::*;
 
+        match *go {
+            Type { ref name, .. } => {
+                if let Some(module) = name.module.as_ref() {
+                    modules.insert(module);
+                }
+            }
+            Map {
+                ref key, ref value, ..
+            } => {
+                Self::type_imports(key, modules);
+                Self::type_imports(value, modules);
+            }
+            Array { ref inner, .. } => {
+                Self::type_imports(inner, modules);
+            }
+            Interface => {}
+        };
+    }
+
+    fn imports<'a>(tokens: &'a Tokens<'a, Self>) -> Option<Tokens<'a, Self>> {
         let mut modules = BTreeSet::new();
 
         for custom in tokens.walk_custom() {
-            match *custom {
-                Imported { ref module, .. } => {
-                    modules.insert(module);
-                }
-                _ => {}
-            };
+            Self::type_imports(custom, &mut modules);
         }
 
         if modules.is_empty() {
@@ -76,24 +106,41 @@ impl Extra {
 impl<'el> Custom for Go<'el> {
     type Extra = Extra;
 
-    fn format(&self, out: &mut Formatter, _extra: &mut Self::Extra, _level: usize) -> fmt::Result {
+    fn format(&self, out: &mut Formatter, extra: &mut Self::Extra, level: usize) -> fmt::Result {
         use self::Go::*;
 
         match *self {
-            Imported {
-                ref module,
-                ref name,
+            Type {
+                name:
+                    Name {
+                        ref module,
+                        ref name,
+                        ..
+                    },
                 ..
             } => {
-                if let Some(module) = module.split("/").last() {
+                if let Some(module) = module.as_ref().and_then(|m| m.as_ref().split("/").last()) {
                     out.write_str(module)?;
                     out.write_str(SEP)?;
                 }
 
                 out.write_str(name)?;
             }
-            Local { ref name } => {
-                out.write_str(name)?;
+            Map {
+                ref key, ref value, ..
+            } => {
+                out.write_str("map[")?;
+                key.format(out, extra, level + 1)?;
+                out.write_str("]")?;
+                value.format(out, extra, level + 1)?;
+            }
+            Array { ref inner, .. } => {
+                out.write_str("[")?;
+                out.write_str("]")?;
+                inner.format(out, extra, level + 1)?;
+            }
+            Interface => {
+                out.write_str("interface{}")?;
             }
         }
 
@@ -144,26 +191,60 @@ impl<'el> Custom for Go<'el> {
 /// Setup an imported element.
 pub fn imported<'a, M, N>(module: M, name: N) -> Go<'a>
 where
-    M: Into<Cow<'a, str>>,
-    N: Into<Cow<'a, str>>,
+    M: Into<Cons<'a>>,
+    N: Into<Cons<'a>>,
 {
-    Go::Imported {
-        module: module.into(),
-        name: name.into(),
+    Go::Type {
+        name: Name {
+            module: Some(module.into()),
+            name: name.into(),
+        },
     }
 }
 
 /// Setup a local element.
 pub fn local<'a, N>(name: N) -> Go<'a>
 where
-    N: Into<Cow<'a, str>>,
+    N: Into<Cons<'a>>,
 {
-    Go::Local { name: name.into() }
+    Go::Type {
+        name: Name {
+            module: None,
+            name: name.into(),
+        },
+    }
+}
+
+/// Setup a map.
+pub fn map<'a, K, V>(key: K, value: V) -> Go<'a>
+where
+    K: Into<Go<'a>>,
+    V: Into<Go<'a>>,
+{
+    Go::Map {
+        key: Box::new(key.into()),
+        value: Box::new(value.into()),
+    }
+}
+
+/// Setup an array.
+pub fn array<'a, I>(inner: I) -> Go<'a>
+where
+    I: Into<Go<'a>>,
+{
+    Go::Array {
+        inner: Box::new(inner.into()),
+    }
+}
+
+/// Setup an interface.
+pub fn interface<'a>() -> Go<'a> {
+    Go::Interface
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{imported, Extra, Go};
+    use super::{array, imported, interface, map, Extra, Go};
     use {Quoted, Tokens};
 
     #[test]
@@ -183,6 +264,36 @@ mod tests {
 
         assert_eq!(
             Ok("package foo\n\nimport \"foo\"\n\nfoo.Debug\n"),
+            toks.to_file_with(Extra::from_package("foo"))
+                .as_ref()
+                .map(|s| s.as_str())
+        );
+    }
+
+    #[test]
+    fn test_map() {
+        let keyed = map(imported("foo", "Debug"), interface());
+
+        let mut toks: Tokens<Go> = Tokens::new();
+        toks.push(toks!(&keyed));
+
+        assert_eq!(
+            Ok("package foo\n\nimport \"foo\"\n\nmap[foo.Debug]interface{}\n"),
+            toks.to_file_with(Extra::from_package("foo"))
+                .as_ref()
+                .map(|s| s.as_str())
+        );
+    }
+
+    #[test]
+    fn test_array() {
+        let keyed = array(imported("foo", "Debug"));
+
+        let mut toks: Tokens<Go> = Tokens::new();
+        toks.push(toks!(&keyed));
+
+        assert_eq!(
+            Ok("package foo\n\nimport \"foo\"\n\n[]foo.Debug\n"),
             toks.to_file_with(Extra::from_package("foo"))
                 .as_ref()
                 .map(|s| s.as_str())
